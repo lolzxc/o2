6c9f1b1019fc2ad0a4d15e2acdeea235455b6a12
lolzxc <guitarbeginner2@gmail.com>
2022-04-28 12:20:09 +0800

    Initial Commit

---
+++ app/src/main/java/com/techsphere/o2/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.techsphere.o2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val myRef = Firebase.database.getReference("Test")
+
+        myRef.setValue("Database is working")
+    }
+}
